0588b6ae74fdee25072078a6ba1e637aa15327a6
Brad Aagaard <baagaard@usgs.gov>
2026-01-22 14:43:58 -0700

    TEMPORARY: Add debugging output.

--- libsrc/pylith/faults/TopologyOps.cc
+++ libsrc/pylith/faults/TopologyOps.cc
@@ -466,6 +466,15 @@ pylith::faults::TopologyOps::updateCohesiveLabel(const pylith::topology::Mesh* m
         err = ISRestoreIndices(pointIS, &points);PYLITH_CHECK_ERROR(err);
         err = ISDestroy(&pointIS);PYLITH_CHECK_ERROR(err);
     } // if
+
+    { // DEBUGGING
+        pythia::journal::debug_t debug("initialize_mesh");
+        if (debug.state() || true) {
+            mesh->view(":mesh_domain_before_orientlabel.txt:ascii_info_detail");
+            mesh->view(":mesh_domain_before_orientlabel.tex:ascii_latex");
+        } // if
+    } // DEBUGGING
+
     err = DMLabelDestroyIndex(dmLabel);PYLITH_CHECK_ERROR(err); // :KLUDGE: Clear out old indexing.
     err = DMPlexOrientLabel(dmMesh, dmLabel);PYLITH_CHECK_ERROR(err);
     err = DMPlexLabelCohesiveComplete(dmMesh, dmLabel, nullptr, 0, PETSC_FALSE, PETSC_FALSE, NULL);PYLITH_CHECK_ERROR(err);
